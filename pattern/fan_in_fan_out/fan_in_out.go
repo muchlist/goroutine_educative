4a896aa47d9e5ddadcf7f735e39433f4ff7078db
mrtsdc <whois.muchlis@gmail.com>
2020-12-25 19:39:08 +0800

    fan in out pattern

--- pattern/fan_in_fan_out/fan_in_out.go
+++ pattern/fan_in_fan_out/fan_in_out.go
@@ -3,70 +3,46 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strconv"
 	"time"
 )
 
-/*
-Not work
-*/
+func updatePosition(name string) <-chan string {
+	positionChannel := make(chan string)
 
-func main() {
-	var myNumbers [10]int
-	for i := 0; i < 10; i++ {
-		rand.Seed(time.Now().UnixNano())
-		myNumbers[i] = rand.Intn(50)
-	}
-
-	mychannelOut := channelGenerator(myNumbers)
-
-	mychannel1 := double(mychannelOut)
-	mychannel2 := double(mychannelOut)
-
-	mychannelIn := fanIn(mychannel1, mychannel2)
-
-	for i := 0; i < len(myNumbers); i++ {
-		fmt.Println(<-mychannelIn)
-	}
-}
-
-func channelGenerator(numbers [10]int) <-chan string {
-	channel := make(chan string)
 	go func() {
-		for _, i := range numbers {
-			channel <- strconv.Itoa(i)
+		for i := 0; ; i++ {
+			positionChannel <- fmt.Sprintf("%s %d", name, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 		}
-		close(channel)
 	}()
-	return channel
+
+	return positionChannel
 }
 
-func double(inputchannel <-chan string) <-chan string {
-	channel := make(chan string)
-	go func() {
-		for i := range inputchannel {
-			num, err := strconv.Atoi(i)
-			if err != nil {
+func fanIn(mychannel1, mychannel2 <-chan string) <-chan string {
+	mychannel := make(chan string)
 
-			}
-			channel <- fmt.Sprintf("%d * 2 = %d", num, num*2)
-		}
-		close(channel)
-	}()
-	return channel
-}
-
-func fanIn(inputchannel1, inputchannel2 <-chan string) <-chan string {
-	channel := make(chan string)
 	go func() {
 		for {
-			select {
-			case message1 := <-inputchannel1:
-				channel <- message1
-			case message2 := <-inputchannel2:
-				channel <- message2
-			}
+			mychannel <- <-mychannel1
 		}
 	}()
-	return channel
+
+	go func() {
+		for {
+			mychannel <- <-mychannel2
+		}
+	}()
+
+	return mychannel
+}
+
+func main() {
+	positionsChannel := fanIn(updatePosition("Legolas :"), updatePosition("Gandalf :"))
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-positionsChannel)
+	}
+
+	fmt.Println("Done with getting updates on positions.")
 }
