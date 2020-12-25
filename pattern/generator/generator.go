0905b012a253c73742a64134d35c242593c35c78
mrtsdc <whois.muchlis@gmail.com>
2020-12-25 18:07:27 +0800

    generator pattern fibo

--- pattern/generator/generator.go
+++ pattern/generator/generator.go
@@ -14,7 +14,23 @@ func updatePosition(name string) <-chan string {
 	return positionChannel
 }
 
+func fibonacci(n int) chan int {
+	myChannel := make(chan int)
+	go func() {
+		k := 0
+		for i, j := 0, 1; k < n; k++ {
+			myChannel <- i
+			i, j = i+j, i
+
+		}
+		close(myChannel)
+	}()
+	return myChannel
+}
+
 func main() {
+
+	//GENERATOR 1
 	positionChannel1 := updatePosition("Legolas :")
 	positionChannel2 := updatePosition("Gandalf :")
 
@@ -37,5 +53,24 @@ func main() {
 		Legolas : 4
 		Gandalf : 4
 		Done with getting updates on positions.
+	*/
+
+	//GENERATOR 2 FIBONACI
+	for i := range fibonacci(10) {
+		//do anything with the nth term while the fibonacci()
+		//is computing the next term
+		fmt.Println(i)
+	}
+
+	/*
+		0
+		1
+		1
+		2
+		3
+		5
+		8
+		13
+		21
 	*/
 }
